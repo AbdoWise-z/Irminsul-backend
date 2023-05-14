304df9d4ba3d449494f0d95ddaaa41131e2a36cf
Elkapper <14712020100527@stud.cu.ed>
2023-05-14 15:26:15 +0200

    wowowow

--- src/main/java/uni/apt/server/TestHandler.java
+++ src/main/java/uni/apt/server/TestHandler.java
@@ -1,18 +1,21 @@
 package uni.apt.server;
 
+import org.springframework.http.MediaType;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 public class TestHandler {
-    static int id = 0;
-    @GetMapping("/test")
-    public String lol(@RequestParam Map<String,String> allRequestParams, ModelMap model){
-        System.out.println("Request received " + (id++) + "\n");
-        return "get gud: " + allRequestParams.get("q");
+
+    @PostMapping(
+            value = "/test",
+            consumes = {MediaType.ALL_VALUE}
+    )
+    public String lol(@RequestBody String str){
+        System.out.println(str);
+
+        return "nothing for you";
     }
 }
