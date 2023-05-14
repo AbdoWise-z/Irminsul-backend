d6053a1ea681cd48daa53e7fd6ae557ef2a4a29f
AbdoWise-z <moh2002.abdow@gmail.com>
2023-05-14 16:59:58 +0300

    finish without bounce

--- src/main/java/uni/apt/server/TestHandler.java
+++ src/main/java/uni/apt/server/TestHandler.java
@@ -1,6 +1,5 @@
 package uni.apt.server;
 
-import org.springframework.http.MediaType;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,10 +8,7 @@ import java.util.Map;
 @RestController
 public class TestHandler {
 
-    @PostMapping(
-            value = "/test",
-            consumes = {MediaType.ALL_VALUE}
-    )
+    @PostMapping("/test")
     public String lol(@RequestBody String str){
         System.out.println(str);
 
